f1385507f7c44ce112d73e61c8e21c1046f42883
KYHSGeekCode <jourhyang123@gmail.com>
2021-08-25 17:58:49 +0900

    :pencil: comment

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ for x in range(minwidth):
         pix = [pix1, pix1, pix2][random.randint(0, 2)]
         # (int(pix1[0] * pix2[0] / 256), int(pix1[1] * pix2[1] / 256), int(pix1[2] * pix2[2] / 256),255)
         pixelsnew[x, y] = (pix[0], pix[1], pix[2], 255)
-
+        # 주석 
 
 newimg.show()
 
